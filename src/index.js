e502207eb5f959316967fbb3bca2f67dc37e3ed1
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-11 12:42:51 +0000

    Connect to MongoDB

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
